1fe72074fdfc38fd43e42fb4e06f2b500ef69e05
Wojciech Szoda <wojciechszoda@icloud.com>
2023-12-07 11:03:45 +0100

    Files to previous commit

---
+++ Library.API/AuthenticationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.API
+{
+    public class AuthenticationSettings
+    {
+        public string JwtKey { get; set; } = String.Empty;
+        public int JwtExpireDays { get; set; }
+        public string JwtIssuer { get; set; } = String.Empty;
+    }
+}
